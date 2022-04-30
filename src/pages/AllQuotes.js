8cab36321b5a80db1e9e9b621cb8261639c2221f
Kasia Lewandowska <lewandowska.k95@gmail.com>
2022-04-30 15:17:43 +0200

    Adding Dummy Data

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Kasia", text: "Learning React is fun!" },
+  { id: "q1", author: "Patryk", text: "Learning React is great!" },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
